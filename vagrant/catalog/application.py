e3ffc64c29edde4371d1dd7610c74b98f412ffe3
markhorvath <markhorvath7@gmail.com>
2017-07-17 22:59:42 -0400

    added newcategory function and html, song info page

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -23,20 +23,40 @@ DBSession = sessionmaker(bind=engine)
 
 session = DBSession()
 
+# Route to main page
 @app.route('/')
 @app.route('/index/')
 def showCategories():
     tags = session.query(Tags).group_by(Tags.tags).all()
-    return render_template('main.html', categories = tags)
+    for i in tags:
+        i.tags = i.tags.title()
+    return render_template('index.html', categories = tags)
 
+# Route to specific category songs
 @app.route('/index/<category>/')
 def showCategorySongs(category):
     ids = session.query(Tags.song_id).filter(Tags.tags.like(category)).all()
-    ids = [r[0] for r in ids]
-    print ids
-#    ids = [3,4,8,16]
+    #change format of ids, there's surely a better way to do this through
+    #a join or subquery
+    ids = [i[0] for i in ids]
     names = session.query(Names).filter(Names.song_id.in_(ids)).all()
     return render_template('categorysongs.html', songs = names, category = category)
+
+# Route to song details
+@app.route('/index/<category>/<int:song_id>/<name>')
+def showSongInfo(category, song_id, name):
+    song = session.query(Song).filter(Song.id == song_id).one()
+    return render_template('songinfo.html', song = song, name = name)
+
+@app.route('/index/newcategory/', methods = ['GET','POST'])
+def newCategory():
+    if request.method == 'POST':
+        newCategory = Tags(tags = request.form['name'])
+        session.add(newCategory)
+        session.commit()
+        return redirect(url_for('showCategories'))
+    else:
+        return render_template('newcategory.html')
 
 def getCategory(query):
     results = session.query(Tags.tags).filter(Tags.tags.like('%query%')).all()
