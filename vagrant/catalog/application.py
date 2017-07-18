6e1a10b072164a9df0901fee800f133b3e369337
markhorvath <markhorvath7@gmail.com>
2017-07-18 16:46:32 -0400

    started login with google, probably going to redo database

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -12,7 +12,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import and_, distinct, func, or_
 
+from flask import session as login_session
+import random, string
+
+#from oauth2client.client import flow_from_clientsecrets
+#from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+import requests
+from flask import make_response
+
 from songsdb_setup import Base, Song, Tags, Names
+
+CLIENT_ID = json.loads(
+        open('client_secrets.json', 'r').read())['web']['client_id']
 
 engine = create_engine('sqlite:///songs.db')
 # Bind the engine to the metadata of the Base class so that the
@@ -23,6 +36,14 @@ DBSession = sessionmaker(bind=engine)
 
 session = DBSession()
 
+# Route to login
+@app.route('/login/')
+def login():
+    state = ''.join(random.choice(string.ascii_uppercase + string.
+                digits) for x in xrange(32))
+    login_session['state'] = state
+    return render_template('login.html', STATE = state)
+    
 # Route to main page
 @app.route('/')
 @app.route('/index/')
@@ -48,6 +69,7 @@ def showSongInfo(category, song_id, name):
     song = session.query(Song).filter(Song.id == song_id).one()
     return render_template('songinfo.html', song = song, name = name)
 
+# Route to add a new category
 @app.route('/index/newcategory/', methods = ['GET','POST'])
 def newCategory():
     if request.method == 'POST':
@@ -58,6 +80,20 @@ def newCategory():
     else:
         return render_template('newcategory.html')
 
+# Route to delete a category
+@app.route('/index/<category>/delete', methods = ['GET', 'POST'])
+def deleteCategory(category):
+    # This only works when 'one.()' is appened, if it's all it returns a 302 error
+    categoryToDelete = session.query(Tags).filter(Tags.tags == category).one()
+    if request.method == 'POST':
+        print categoryToDelete
+        session.delete(categoryToDelete)
+        session.commit()
+        return redirect(url_for('showCategories'))
+    else:
+        print categoryToDelete
+        return render_template('deletecategory.html', item = categoryToDelete)
+
 def getCategory(query):
     results = session.query(Tags.tags).filter(Tags.tags.like('%query%')).all()
     return results
@@ -66,5 +102,6 @@ def getCategory(query):
 #print results
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host = '0.0.0.0', port = 8000)
